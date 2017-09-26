215390ea61dcee3fde467171740e22d6ee9c405e
matr0s <serhiy.osadchuk@gmail.com>
2017-09-26 23:09:58 +0300

    Step 2 update for testing

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 (function(){
 //// Question protorype
+var n, userAnswer;
+var answerCounter = 0;
 var Question = function (question, answers, correctAnswer){
  this.question = question;
  this.answers = answers;
@@ -15,26 +17,40 @@ Question.prototype.questionPrint = function() {
  }
 
 Question.prototype.compareAnswer = function(){
-if (userAnswer === this.correctAnswer) {
+if (userAnswer == this.correctAnswer) {
 	console.log('Correct answer. My congratulations!');
+	answerCounter ++;
 } else {
 	console.log('Wrong answer. Looks like You are loooooser!!!');
 }
+	console.log('Your total score is: ' + answerCounter);
+	console.log('=============================================');
 }
+
 /// Init
 var q0 = new Question('Do you see this question?', ['Yes', 'No', 'Hz'], 0);
 var q1 = new Question('Whos is developer here?', ['Petro', 'Serhii', 'Hz'], 1);
-var q2 = new Question('Just another question with correct ansfer "Hz"', ['Yes', 'Hz'], 1);
+var q2 = new Question('Just another question with correct ansfer "Hz"', ['Yes', 'Hz', 'Hzzz'], 1);
 var q3 = new Question('What is DOIT?', ['Dev Company', 'Nike Moto', 'Hz'], 0);
 /// Array with questions creation
 var questionsArray = [q0, q1, q2, q3];
 
-/// Get random question
-var n = Math.floor((Math.random() * (questionsArray.length)));
-questionsArray[n].questionPrint();
-
+while (true)
+{
+if (userAnswer === 'exit') {
+	console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	break;
+} else { 
+randomQuestion();
+}
+}
 ////Main block with code execution here
-var userAnswer = parseInt(prompt('Please input your answer (number) or type "exit" to quit'));
-questionsArray[n].compareAnswer();
 
+/// Get random question
+function randomQuestion() {
+n = Math.floor((Math.random() * (questionsArray.length)));
+questionsArray[n].questionPrint();	
+userAnswer = prompt('Please input your answer (number) or type "exit" to quit');
+questionsArray[n].compareAnswer();
+}
 })();
