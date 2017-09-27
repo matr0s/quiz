649e81b47d687f851d735ff8ef56ff141d4684c9
matr0s <serhiy.osadchuk@gmail.com>
2017-09-27 22:03:53 +0300

    Finall update for the step2

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
 (function(){
 //// Question protorype
-var n, userAnswer;
-var answerCounter = 0;
 var Question = function (question, answers, correctAnswer){
  this.question = question;
  this.answers = answers;
@@ -10,20 +8,26 @@ var Question = function (question, answers, correctAnswer){
 }
 
 Question.prototype.questionPrint = function() {
- 		console.log('Question #' + (n+1) + ': ' + this.question);
- 		for (var j = 0; j < this.answers.length; j++) {
-			console.log(j + ': ' + this.answers[j]);
+ 	console.log('Question: ' + this.question);
+ 	for (var j = 0; j < this.answers.length; j++) {
+		console.log(j + ': ' + this.answers[j]);
 		}
- }
-
-Question.prototype.compareAnswer = function(){
-if (userAnswer == this.correctAnswer) {
-	console.log('Correct answer. My congratulations!');
-	answerCounter ++;
-} else {
-	console.log('Wrong answer. Looks like You are loooooser!!!');
 }
-	console.log('Your total score is: ' + answerCounter);
+
+Question.prototype.compareAnswer = function(ua, callback){
+	var sc;
+	if (ua === this.correctAnswer) {
+		console.log('Correct answer. My congratulations!');
+		sc = callback(true);
+	} else {
+		console.log('Wrong answer. Looks like You are loooooser!!!');
+		sc = callback(false);
+		}
+	this.displayScore(sc);
+}
+
+Question.prototype.displayScore = function(score) {
+	console.log('Your total score is: ' + score);
 	console.log('=============================================');
 }
 
@@ -32,25 +36,30 @@ var q0 = new Question('Do you see this question?', ['Yes', 'No', 'Hz'], 0);
 var q1 = new Question('Whos is developer here?', ['Petro', 'Serhii', 'Hz'], 1);
 var q2 = new Question('Just another question with correct ansfer "Hz"', ['Yes', 'Hz', 'Hzzz'], 1);
 var q3 = new Question('What is DOIT?', ['Dev Company', 'Nike Moto', 'Hz'], 0);
-/// Array with questions creation
 var questionsArray = [q0, q1, q2, q3];
 
-while (true)
-{
-if (userAnswer === 'exit') {
-	console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	break;
-} else { 
-randomQuestion();
-}
-}
 ////Main block with code execution here
+var keepScore = score();
+randomQuestion();
 
-/// Get random question
+/// FUNCTIONS
 function randomQuestion() {
-n = Math.floor((Math.random() * (questionsArray.length)));
-questionsArray[n].questionPrint();	
-userAnswer = prompt('Please input your answer (number) or type "exit" to quit');
-questionsArray[n].compareAnswer();
+	var n = Math.floor((Math.random() * (questionsArray.length)));
+	questionsArray[n].questionPrint();	
+	var userAnswer = prompt('Please input your answer (number) or type "exit" to quit');
+		if (userAnswer !== 'exit') {
+			questionsArray[n].compareAnswer(parseInt(userAnswer), keepScore);
+			randomQuestion();	
+			}
+}
+
+function score() {
+	var sc = 0;
+	return function (correct) {
+		if (correct) {
+			sc++;
+		}
+	return sc;
+	}
 }
 })();
